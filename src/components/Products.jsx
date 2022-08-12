89723bd0fcb753201c52a1165d65cf082daded8c
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2022-08-12 17:56:04 -0300

    feat: products e initialstate

---
+++ src/components/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Product from './Product'
+import '../styles/components/Products.css'
+
+function Products({ products }) {
+  return (
+    <div className="Products">
+      <div className="Products-items">
+        {products.map(e => (
+            <Product key={e.id} product={e} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Products
