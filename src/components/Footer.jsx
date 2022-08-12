e1089b420906e71ad2c9666a8d1c5514be107ff6
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2022-08-12 17:42:13 -0300

    feat: layout y state

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/components/Footer.css';
+
+function Footer() {
+  return (
+    <div className='Footer'>
+      <p className="Footer-title">Platzi conf merch</p>
+      <p className="Footer-copy">Derechos reservados</p>
+    </div>
+  )
+}
+
+export default Footer
